26a3df00b2bcfdb70f3ddf64f62c4a06fb299180
Takeshi KOMIYA <i.tkomiya@gmail.com>
2013-02-11 17:43:11 +0900

    Fix foodcritic violations

--- metadata.rb
+++ metadata.rb
@@ -1,3 +1,4 @@
+name             "cassandra-bin"
 maintainer       "Takeshi KOMIYA"
 maintainer_email "i.tkomiya@gmail.com"
 license          "Apache 2.0"
--- recipes/default.rb
+++ recipes/default.rb
@@ -11,11 +11,6 @@ include_recipe "java"
 
 version = node['cassandra-bin']['version']
 
-execute "cassandra restart" do
-  command "/etc/init.d/cassandra restart"
-  action :nothing
-end
-
 service "cassandra" do
   supports :restart => true
   action :nothing
@@ -27,7 +22,7 @@ template "/usr/local/apache-cassandra-#{version}/conf/cassandra.yaml" do
   group "root"
   mode "0644"
   only_if {::File.exists?("/usr/local/apache-cassandra-#{version}/conf")}
-  notifies :run, resources(:execute => "cassandra restart")
+  notifies :restart, "service[cassandra]"
 end
 
 template "/etc/init.d/cassandra" do
@@ -38,8 +33,8 @@ template "/etc/init.d/cassandra" do
   variables(
     :cassandra_version => version
   )
-  notifies :enable, resources(:service => "cassandra"), :immediately
-  notifies :run, resources(:execute => "cassandra restart")
+  notifies :enable, "service[cassandra]", :immediately
+  notifies :restart, "service[cassandra]"
 end
 
 script "install cassandra" do
@@ -51,15 +46,15 @@ script "install cassandra" do
     tar xzf #{Chef::Config[:file_cache_path]}/apache-cassandra-#{version}-bin.tar.gz
   EOH
   action :nothing
-  notifies :create, resources(:template => "/usr/local/apache-cassandra-#{version}/conf/cassandra.yaml"), :immediately
-  notifies :create, resources(:template => "/etc/init.d/cassandra"), :immediately
-  notifies :run, resources(:execute => "cassandra restart"), :immediately
+  notifies :create, "template[/usr/local/apache-cassandra-#{version}/conf/cassandra.yaml]"), :immediately
+  notifies :create, "template[/etc/init.d/cassandra]", :immediately
+  notifies :restart, "service[cassandra]", :immediately
 end
 
 remote_file "#{Chef::Config[:file_cache_path]}/apache-cassandra-#{version}-bin.tar.gz" do
   source "#{node['cassandra-bin']['url']}/#{version}/apache-cassandra-#{version}-bin.tar.gz"
   not_if {::File.exists?("/usr/local/apache-cassandra-#{version}")}
-  notifies :run, resources(:script => "install cassandra"), :immediately
+  notifies :run, "script[install cassandra]", :immediately
 end
 
 file "cassandra-tarball-cleanup" do
