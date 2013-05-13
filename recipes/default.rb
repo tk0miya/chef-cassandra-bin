3c11810ffcf3d48653d6c73838be372b4ccf5b59
Takeshi KOMIYA <i.tkomiya@gmail.com>
2013-05-13 18:54:34 +0900

    Refactored

--- recipes/default.rb
+++ recipes/default.rb
@@ -11,18 +11,21 @@ include_recipe "java"
 
 version = node['cassandra-bin']['version']
 
-service "cassandra" do
-  supports :restart => true
-  action :nothing
+remote_file "#{Chef::Config[:file_cache_path]}/apache-cassandra-#{version}-bin.tar.gz" do
+  source "#{node['cassandra-bin']['url']}/#{version}/apache-cassandra-#{version}-bin.tar.gz"
+  not_if {::File.exists?("/usr/local/apache-cassandra-#{version}")}
+  notifies :run, "script[install cassandra]", :immediately
 end
 
-template "/usr/local/apache-cassandra-#{version}/conf/cassandra.yaml" do
-  source "cassandra.yaml.erb"
-  owner "root"
-  group "root"
-  mode "0644"
-  only_if {::File.exists?("/usr/local/apache-cassandra-#{version}/conf")}
-  notifies :restart, "service[cassandra]"
+script "install cassandra" do
+  interpreter "bash"
+  user "root"
+  cwd "/usr/local"
+  code <<-EOH
+    tar xzf #{Chef::Config[:file_cache_path]}/apache-cassandra-#{version}-bin.tar.gz
+  EOH
+  action :nothing
+  notifies :create, "template[/usr/local/apache-cassandra-#{version}/conf/cassandra.yaml]"
 end
 
 template "/etc/init.d/cassandra" do
@@ -33,28 +36,20 @@ template "/etc/init.d/cassandra" do
   variables(
     :cassandra_version => version
   )
-  notifies :enable, "service[cassandra]", :immediately
+end
+
+template "/usr/local/apache-cassandra-#{version}/conf/cassandra.yaml" do
+  source "cassandra.yaml.erb"
+  owner "root"
+  group "root"
+  mode "0644"
+  only_if {::File.exists?("/usr/local/apache-cassandra-#{version}")}
   notifies :restart, "service[cassandra]"
 end
 
-script "install cassandra" do
-  interpreter "bash"
-  only_if {::File.exists?("#{Chef::Config[:file_cache_path]}/apache-cassandra-#{version}-bin.tar.gz")}
-  user "root"
-  cwd "/usr/local"
-  code <<-EOH
-    tar xzf #{Chef::Config[:file_cache_path]}/apache-cassandra-#{version}-bin.tar.gz
-  EOH
-  action :nothing
-  notifies :create, "template[/usr/local/apache-cassandra-#{version}/conf/cassandra.yaml]", :immediately
-  notifies :create, "template[/etc/init.d/cassandra]", :immediately
-  notifies :restart, "service[cassandra]", :immediately
-end
-
-remote_file "#{Chef::Config[:file_cache_path]}/apache-cassandra-#{version}-bin.tar.gz" do
-  source "#{node['cassandra-bin']['url']}/#{version}/apache-cassandra-#{version}-bin.tar.gz"
-  not_if {::File.exists?("/usr/local/apache-cassandra-#{version}")}
-  notifies :run, "script[install cassandra]", :immediately
+service "cassandra" do
+  action [:enable, :start]
+  supports :restart => true
 end
 
 file "cassandra-tarball-cleanup" do
